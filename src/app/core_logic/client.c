0a4981396f3eb4af1707b2f094e7b44cf0d67e96
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2023-10-16 22:53:33 +0200

    start sae
    Signed-off-by: Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>

---
+++ src/app/core_logic/client.c
@@ -0,0 +1,26 @@
+//
+// Created by Mathéo Hersan on 16/10/2023.
+//
+
+#include "client.h"
+
+void afficherDonneesClient() {
+    // Ouverture du fichier en mode lecture
+    FILE *fichier = fopen("donnee/client.txt", "r");
+    int numeroClient;
+    float cagnotte;
+    int suspendu;
+
+    if (fichier == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fscanf(fichier, "%d%f%d", &numeroClient, &cagnotte, &suspendu) == 3) {
+        printf("Client %d, Cagnotte %.2f, Suspendu: %s\n",
+               numeroClient, cagnotte, (suspendu == 0) ? "Non" : "Oui");
+    }
+
+    // Fermeture du fichier
+    fclose(fichier);
+}
--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 #include "app/interface/interface_client.h"
 #include "app/interface/interface_resp.h"
+#include "app/core_logic/client.h"
 
 int main(){
 
     affiche_client();
+    afficherDonneesClient();
 }
