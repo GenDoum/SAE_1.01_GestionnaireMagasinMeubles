a2a2577bafb921145dc5d993c8dfc51a41679fde
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2023-10-16 09:49:39 +0200

    start of client fonctions

---
+++ src/app/interface/client/interface_client.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void affiche_client()
+{
+    printf("\n");
+    printf("--------------- \n");
+    printf("|| Bonjour ! ||\n") ;
+    printf("--------------- \n");
+    printf("\n");
+    printf("-------------------------------------------------------------------\n");
+    printf("|| Que voulez-vous faire ? \t \t \t \t \t || \n") ;
+    printf("||\t1 : Afficher le récapitulatif de votre panier. \t \t \t \t || \n") ;
+    printf("-------------------------------------------------------------------\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "app/interface/client/interface_client.h"
+#include "app/interface/resp/interface_resp.h"
 
 int main(){
     printf("Hello World\n");
