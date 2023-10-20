0d9eac6e561aaca9cb0a203fcc957d23be65613f
mahersan <Matheo.HERSAN@etu.uca.fr>
2023-10-20 17:15:19 +0200

    Added chargerClient

--- src/app/core_logic/client.c
+++ src/app/core_logic/client.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
 //fonction ajouter un article au panier.
+#include "client.h"
+
+void chargerClient(int tNumClient[], float tCagnotte[], int tSus[], int *tLogique)
+{
+    FILE *fic;
+    int i;
+    fic = fopen("donnee/client.txt", "r");
+    if (fic == NULL)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fscanf(fic, "%d %f %d", &tNumClient[*tLogique], &tCagnotte[*tLogique], &tSus[*tLogique]) != EOF)
+    {
+        (*tLogique)++;
+    }
+    fclose(fic);
+}
--- src/app/core_logic/client.h
+++ src/app/core_logic/client.h
@@ -8,6 +8,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void ajouter_article(int reference, float poids, float volume, int prixUnitaire);
+void chargerClient(int tNumClient[], float tCagnotte[], int tSus[], int *tLogique);
 
 #endif //SAE_101_CLIENT_H
