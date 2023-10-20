56dcf72d3ce29efaf527de028607dcdfde4502cb
Yannis Doumir Fernandes <Yannis.DOUMIR_FERNANDES@etu.uca.fr>
2023-10-20 17:31:37 +0200

    ADD chargementArticles()

---
+++ src/app/core_logic/responsable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "responsable.h"
+
+int chargementArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], float tPhysique)
+{
+    int i=0, ref;
+    float volume, poids, prix;
+    FILE * fe;
+    fe = fopen("donnee/articles.txt", "r");
+    if ( fe == NULL)
+    {
+        perror("fopen");
+    }
+    while ( fscanf(fe,"%d %f %f %f", &ref, &poids, &volume, &prix) == 4)
+    {
+        if ( i == tPhysique )
+        {
+            fprintf(stderr,"Tableau plein");
+        }
+        tRef[i] = ref;
+        tPoids[i] = poids;
+        tVol[i] = volume;
+        tPrix[i] = prix;
+        i++;
+    }
+    return i;
+}
---
+++ src/app/core_logic/responsable.h
@@ -0,0 +1,3 @@
+
+
+int chargementArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], float tPhysique);
