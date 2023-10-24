8122a29913f623e5cbc82e5446d97ec2500587bc
Yannis DOUMIR FERNANDES <yannis.doumir_fernandes@etu.uca.fr>
2023-10-24 08:59:49 +0200

    Mise à jour de 'src/app/core_logic/responsable.h'

--- src/app/core_logic/responsable.h
+++ src/app/core_logic/responsable.h
@@ -1,3 +1,4 @@
 
 
 int chargementArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], int tPhysique);
+void sauvegadArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], float tLogique);
