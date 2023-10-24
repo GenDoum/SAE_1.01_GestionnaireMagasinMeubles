f05a7d73d4d435b9c0dee34ae2cecc6143a9e050
Yannis DOUMIR FERNANDES <yannis.doumir_fernandes@etu.uca.fr>
2023-10-24 09:08:14 +0200

    Mise à jour de 'src/app/interface/interface_client.c'

--- src/app/interface/interface_client.c
+++ src/app/interface/interface_client.c
@@ -47,14 +47,3 @@ void global_client(){
     }
 }
 
-void affichAjoutArticle(int *ref, float *poids, float *volume, float *prix)
-{
-    printf("Entrez la ref du nouveaux produit");
-    scanf("%d", ref);
-    printf("Entrez le poids du nouveaux produit");
-    scanf("%f", poids);
-    printf("Entrez le volume du nouveaux produit");
-    scanf("%f", volume);
-    printf("Entrez le prix du nouveaux produit");
-    scanf("%f", prix);
-}
