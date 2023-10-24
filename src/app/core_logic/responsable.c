a232db8ffe344615ec5fe7fb4cf2242d9848f806
Yannis DOUMIR FERNANDES <yannis.doumir_fernandes@etu.uca.fr>
2023-10-24 08:59:27 +0200

    Mise à jour de 'src/app/core_logic/responsable.c'

--- src/app/core_logic/responsable.c
+++ src/app/core_logic/responsable.c
@@ -26,3 +26,22 @@ int chargementArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], 
     }
     return i;
 }
+
+void sauvegadArticles(int tRef[], float tPoids[], float tVol[], float tPrix[], float tLogique)
+{
+    int i;
+    FILE * fe;
+    fe = fopen("articles.txt","w");
+    if ( fe == NULL )
+    {
+        perror("fopen");
+        return -1;
+    }
+    
+    for ( i = 0; i < tLogique; ++i)
+    {
+        fwritef(fe,"%d\t %.2f\t %.2f\t %.2f\n", );
+    }
+
+    fclose(fe);
+}
