822faa411a4b0d4e312e492639e41f1c95702a46
Yannis DOUMIR FERNANDES <yannis.doumir_fernandes@etu.uca.fr>
2023-10-24 09:09:03 +0200

    Mise à jour de 'src/app/interface/interface_resp.h'

--- src/app/interface/interface_resp.h
+++ src/app/interface/interface_resp.h
@@ -2,3 +2,4 @@ void affichArticles( int tRef[], float tPoids[], float tVol[], float tPrix[], in
 void affichUnArticle(int tRef[], float tPoids[], float tVol[], float tPrix[], int tLogique, int val);
 void affichUnClient(int tNumClient[], float tCagnotte[], int tSus[], int tLogique, int val);
 void affichUnClients(int tNumClient[], float tCagnotte[], int tSus[], int tLogique, int val);
+void affichAjoutArticle(int *ref, float *poids, float *volume, float *prix);
