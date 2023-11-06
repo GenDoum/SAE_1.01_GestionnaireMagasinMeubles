158ee7bd9717fe858150798d9bc7f6f32bb3beef
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2023-11-06 16:15:08 +0100

    better makefile
    Signed-off-by: Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>

--- docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['deduire_5fcagnotte_0',['deduire_cagnotte',['../client_8c.html#a7203df964d63cb07d7b19912d5e3c3bd',1,'deduire_cagnotte(int numeroClient, float montant, int numeros[], float cagnottes[], int nombreClients, int suspendus[]):&#160;client.c'],['../client_8h.html#a7203df964d63cb07d7b19912d5e3c3bd',1,'deduire_cagnotte(int numeroClient, float montant, int numeros[], float cagnottes[], int nombreClients, int suspendus[]):&#160;client.c']]]
-];
